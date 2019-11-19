72dbf157d43b7566b2bdee694af0459345550eb5
Ehm Kannde <Ehmmannde@gmail.com>
2019-11-19 06:49:50 +0000

    made a comment in my bikeshare2.py file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -20,6 +20,7 @@ def get_filters():
 
 
     # get user input for month (all, january, february, ... , june)
+    # and I added a commet here
 
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
